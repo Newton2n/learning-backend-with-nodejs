a1b749afe18c93a8ff043f855fee23ebbd67fcf6
Newton <156936866+Newton2n@users.noreply.github.com>
2026-02-21 11:55:36 +0600

    Learning debug tools with node js

---
+++ 06-errors-debugging/logical.js
@@ -0,0 +1,10 @@
+const logical = () => {
+  let num = 5;
+  if (num === 5) {
+    console.log(`NUmber is ${num}`);
+  } else {
+    console.log(`num is not ${num}`);
+  }
+};
+
+module.exports = logical;
